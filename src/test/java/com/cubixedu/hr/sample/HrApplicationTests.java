75429774fac0330af8b638656d0522769193607e
imre-gabor <gabor@aut.bme.hu>
2025-11-11 17:30:05 +0100

    prepare for live 1

---
+++ src/test/java/com/cubixedu/hr/sample/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubixedu.hr.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
