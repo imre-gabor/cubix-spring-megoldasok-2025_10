792a6f06dace43b37378aaeff0752a02fc6640d4
imre-gabor <gabor@aut.bme.hu>
2025-11-11 18:44:48 +0100

    solution at live 1

--- src/main/java/com/cubixedu/hr/sample/service/SmartEmployeeService.java
+++ src/main/java/com/cubixedu/hr/sample/service/SmartEmployeeService.java
@@ -2,6 +2,9 @@ package com.cubixedu.hr.sample.service;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,19 +22,44 @@ public class SmartEmployeeService implements EmployeeService {
 	@Override
 	public int getPayRaisePercent(Employee employee) {
 		
-		double yearsWorked = ChronoUnit.DAYS.between(employee.getDateOfStartWork(), LocalDateTime.now()) / 365.0;
-		
+		double yearsWorked = ChronoUnit.DAYS.between(employee.getDateOfStartWork(), LocalDateTime.now()) / 365.0;		
 		Smart smartConfig = config.getSalary().getSmart();
-		if(yearsWorked > smartConfig.getLimit3())
-			return smartConfig.getPercent3();
 		
-		if(yearsWorked > smartConfig.getLimit2())
-			return smartConfig.getPercent2();
+//		if(yearsWorked > smartConfig.getLimit3())
+//			return smartConfig.getPercent3();
+//		
+//		if(yearsWorked > smartConfig.getLimit2())
+//			return smartConfig.getPercent2();
+//		
+//		if(yearsWorked > smartConfig.getLimit1())
+//			return smartConfig.getPercent1();
+
+		//opcionális feladat
+		TreeMap<Double, Integer> limitsMap = smartConfig.getLimits();
+		//1. megoldás
 		
-		if(yearsWorked > smartConfig.getLimit1())
-			return smartConfig.getPercent1();
+//		Integer maxLimit = null;
+//		for(var entry: limitsMap.entrySet()) {
+//			if(yearsWorked > entry.getKey()) {
+//				maxLimit = entry.getValue();
+//			} else {
+//				break;
+//			}
+//		}
+//		
+//		return maxLimit ==  null ? 0 : maxLimit;
 		
-		return 0;
+		//2. megoldás
+//		Optional<Double> optionalMax = limitsMap.keySet()
+//			.stream()
+//			.filter(k -> yearsWorked > k)
+//			.max(Double::compare);
+//		
+//		return optionalMax.isEmpty() ? 0 : limitsMap.get(optionalMax.get());
+	
+		//3. megoldás
+		Entry<Double, Integer> floorEntry = limitsMap.floorEntry(yearsWorked);
+		return floorEntry == null ? 0 : floorEntry.getValue();
 	}
 
 }
